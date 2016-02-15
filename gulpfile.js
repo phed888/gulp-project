ab7f62c55c1f57c72ee7ed43702dfbfc9d7eefff
Brian Speight <brspeight@expedia.com>
2016-02-15 13:23:08 -0800

    Adding bower to the project - with the .bowerrc file at the root level to direct the creation of the 'bower-components' directory to inside of the 'app' directory. Adding jQuery and d3 to the project via Bower. Adding simple test to main.js file - using d3 to add a paragraph w/in body with the text "Hello, there!" Tested and it works

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,14 @@ gulp.task('sass', function() {
 		}))
 });
 
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'app'
+		}
+	})
+});
+
 gulp.task('watch', ['browserSync', 'sass'], function() {
 	gulp.watch('app/scss/**/*.scss', ['sass']);
 	gulp.watch('app/*.html', browserSync.reload);
